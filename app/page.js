1f304b0cea352948f5f93c03b74d5f96c6314588
Harsh Sharma <hs804506@gmail.com>
2025-04-21 22:16:58 +0530

    main page designed

--- app/page.js
+++ app/page.js
@@ -5,16 +5,16 @@ export default function Home() {
   return (
     <>
       {/* <Navbar /> */}
-      <main>
+      <main className="bg-purple-100 text-black">
         <section className="grid grid-cols-2 h-[50vh]">
-          <div className="bg-green-50">
-            <p>The best URL Shortner in the market</p>
-          {/* <div> */}
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-2xl font-bold">The best URL Shortner in the market</p>
+         
             <p>We are the most straight forward URL Shortner in the World</p>
-          {/* </div> */}
+      
           </div>
-          <div className="bg-red-500 flex justify-start relative">
-            <Image src={"/vector.jpg"} alt="an image of a vector"  fill={true} /> 
+          <div className="flex justify-start relative">
+            <Image className="mix-blend-darken" src={"/vector.jpg"} alt="an image of a vector"  fill={true} /> 
           </div>
         </section>
       </main>
